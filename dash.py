ebdcb298c9d2174b1edf324e527551c1c8eb7b94
PaulAandree <84433022+PaulAandree@users.noreply.github.com>
2023-06-08 16:28:34 -0500

    Add files via upload

---
+++ dash.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title=" KPI telecom argentina",
+    page_icon=" ::):"
+)
+
+st.title("Main Page")
+st.sidebar.success("select a page above.")
+
